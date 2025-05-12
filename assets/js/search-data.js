4e4bae73d5ddab0d0cf32a4fa78eb0e8fd267bff
holstritch <100155655+holstritch@users.noreply.github.com>
2025-05-12 21:10:11 +0000

    Deploying to gh-pages from @ holstritch/holstritch.github.io@38dd944de006d72ddb7f703d38d11f0d8379daa5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Below is a breakdown of my CV, it&#39;s also available as a pdf.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
